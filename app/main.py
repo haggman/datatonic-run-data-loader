5f300d3bafba5c58c8c0092c1f362c33c7592196
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-13 11:17:32 -0500

    Test Forecast API access

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+import logging
+import urllib
+import google.auth.transport.requests
+import google.oauth2.id_token
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+    service_url = 'https://api.forecast.it/api/v1/projects'
+    req = urllib.request.Request(service_url)
+
+    auth_req = google.auth.transport.requests.Request()
+    id_token = google.oauth2.id_token.fetch_id_token(auth_req, service_url)
+    req.add_header("Authorization", f"Bearer {id_token}")
+    response = urllib.request.urlopen(req)
+    logging.info('Response: ', response.read())
     return {"message": "Hello World"}
 
 
